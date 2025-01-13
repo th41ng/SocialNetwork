a9247fe7626346d0297e166df488bda2d662fc19
capchencanh <danhvip83@gmail.com>
2025-01-13 11:12:07 +0700

    cap nhat so luoc cau truc mobile

---
+++ socialappmobile/components/Home/Home.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+import MyStyles from "../../styles/MyStyles"
+
+const Home = () => {
+    return (
+        <View style={MyStyles.container}>
+            <Text style={MyStyles.subject}>Trang chu HOME</Text>
+        </View>
+    )
+}
+
+export default Home;
