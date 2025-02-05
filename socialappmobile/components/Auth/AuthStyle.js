cdf743a124c94b74021f95d5c2bc8d060ece5e5b
th41ng <nguyenlethanhthang@gmail.com>
2025-02-05 22:45:35 +0700

    Thang vo Thang

--- socialappmobile/components/Auth/AuthStyle.js
+++ socialappmobile/components/Auth/AuthStyle.js
@@ -18,7 +18,22 @@ const AuthStyle = StyleSheet.create({
     marginTop:10,
     alignItems: "center", 
   },
-
+  imagePicker: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    padding: 10,
+  },
+  
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  
 });
 
 export default AuthStyle;
